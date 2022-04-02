2471db5b27814153a7291393c734255d2caa906a
boevoiverolet <bjha78rus@yandex.ru>
2022-04-02 11:53:40 +0300

    hw1 done

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
-function Message() {
-    return (
-        <div>
 
-        </div>
+type MessageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message(props: MessageDataType) {
+    return (
+        <>
+            <div className='message'>
+                <div className='empty'>
+                    <div className='hack'><img className='image' src={props.avatar} alt={props.name}/></div>
+                </div>
+
+                <div className='content'>
+                    <div className='name'>{props.name}</div>
+
+                    <div className='text'>{props.message}</div>
+                    <div className='time'>{props.time}</div>
+                </div>
+
+            </div>
+
+
+        </>
     )
 }
 
-export default Message
+export default Message;
