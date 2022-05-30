619b07e758e8a41c2a2364442ceeb1d21640d5a1
boevoivertolet <bjha78rus@yandex.ru>
2022-05-30 15:17:08 +0300

    Updates

---
+++ precache-manifest.336886ad1128c54bbbf12f5bba12c70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35bdde3105e14f963f74a21d141f630",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "ee7a5c1b8ee4aa8ef4e3",
+    "url": "/homework/static/css/main.2cca517e.chunk.css"
+  },
+  {
+    "revision": "527b9425c7c069fcaa65",
+    "url": "/homework/static/js/2.1e93cd81.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/homework/static/js/2.1e93cd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7a5c1b8ee4aa8ef4e3",
+    "url": "/homework/static/js/main.1d77cf00.chunk.js"
+  },
+  {
+    "revision": "8c5bfeabec2c84a40778",
+    "url": "/homework/static/js/runtime-main.f38b6f31.js"
+  }
+]);
